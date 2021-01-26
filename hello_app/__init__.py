d10834977aea7e0e67ef4d32fcb965a1626a5be2
Taylor Cowan <tacowan@microsoft.com>
2021-01-26 08:10:28 -0600

    Update __init__.py

--- hello_app/__init__.py
+++ hello_app/__init__.py
@@ -1,2 +1,11 @@
 from flask import Flask  # Import the Flask class
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.ext.flask.flask_middleware import FlaskMiddleware
+from opencensus.trace.samplers import ProbabilitySampler
 app = Flask(__name__)    # Create an instance of the class for our use
+
+middleware = FlaskMiddleware(
+    app,
+    exporter=AzureExporter(connection_string="InstrumentationKey=[your key goes here]"),
+    sampler=ProbabilitySampler(rate=1.0),
+)
